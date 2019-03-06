a95524096018caa77578c3d9aab80d0cc46b5445
rpolicastro <rpolicas@indiana.edu>
2019-03-06 14:47:46 -0500

    fastqc and bowtie2 index

--- MAIN.sh
+++ MAIN.sh
@@ -7,4 +7,31 @@ source SETTINGS.sh
 ## ChIP-seq Analysis of Multiple Samples
 #########################################
 
+### loading conda environment
 
+source activate chipseq
+
+### fastqc of reads
+
+# creating directory to output fastqc results
+mkdir -p ${BASEDIR}/results/fastqc
+
+# saving fastq files variable
+FASTQ_FILES=$(find ${SEQDIR} -name "*fastq")
+
+# running fastqc
+fastqc \
+-t $CORES \
+-o ${BASEDIR}/results/fastqc \
+$FASTQ_FILES
+
+### generating bowtie2 index
+
+# create directory for genomic index
+mkdir -p ${BASEDIR}/genome
+
+# create bowtie2 index
+bowtie2-build \
+-f --threads $CORES \
+$GENOME_FASTA \
+${BASEDIR}/genome/hg38
