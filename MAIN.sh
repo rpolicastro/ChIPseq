779ac114835477041d24fb4f14fd171fc978b2f0
rpolicastro <rpolicas@indiana.edu>
2019-03-06 14:25:25 -0500

    fixing authorship

---
+++ MAIN.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd $PBS_O_WORKDIR
+source SETTINGS.sh
+
+#########################################
+## ChIP-seq Analysis of Multiple Samples
+#########################################
+
+
