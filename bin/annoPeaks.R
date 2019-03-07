a7c0e8442816b1ff54d8c1f022fed2ee4c67416a
rpolicastro <rpolicas@indiana.edu>
2019-03-07 13:55:45 -0500

    finishing up the peaks annotation

--- bin/annoPeaks.R
+++ bin/annoPeaks.R
@@ -25,10 +25,14 @@ opt         <-  parse_args(opt_parser)
 ## peak annotation
 ## ---------------
 
+# ensure the extra columns in the macs2 narrowpeaks are carried over
 extraCols_narrowPeak  <-  c(signal.value = "numeric", p.value = "numeric", q.value = "numeric", peak = "integer")
 
+# go through each narrowpeaks file and annotate the peaks
 for (file in list.files(file.path(opt$outdir, "results", "aligned"), pattern=".*\\.narrowPeak")) {
+	# importing narrowpeaks as GRanges object
 	peaks      <-  import(file, format="BED", extraCols=extraCols_narrowPeak)
+	# ChIPseeker to anntoate peaks using hg38 TxDB and OrgDb objects
 	annotated  <-  annotatePeak(
 		peaks,
 		tssRegion=c(-opt$upstream, opt$downstream),
@@ -36,16 +40,19 @@ for (file in list.files(file.path(opt$outdir, "results", "aligned"), pattern=".*
 		level="gene",
 		annoDb="org.Hs.eg.db",
 	)
-
+	
+	# exporting some of the handy ChIPseeker graphs
 	pdf(file.path(opt$outdir, "results", "peaks_annotated", paste0(tools::file_path_sans_ext(basename(file)), ".tsv"))
 	plotAnnoPie(annotated)
 	plotAnnoBar(annotated)
 	plotDistToTSS(annotated)
 	dev.off()
 
+	# writing annotated peaks to tsv
 	annotated  <- as.data.frame(annotated)
 	write.table(
 		annotated,
 		file.path(opt$outdir, "results", "peaks_annotated", paste0(tools::file_path_sans_ext(basename(file)), ".tsv")),
 		sep="\t", col.names=TRUE, row.names=FALSE, quote=FALSE, na=""
 	)
+}
