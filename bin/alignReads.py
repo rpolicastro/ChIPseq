2fd6a6cb75f736a9d83acdce83dc7ff07a2f871d
rpolicastro <rpolicas@indiana.edu>
2019-03-06 19:47:50 -0500

    finishing sample sheet aware alignment

--- bin/alignReads.py
+++ bin/alignReads.py
@@ -2,19 +2,64 @@
 
 import argparse
 import os
-import 
+import subprocess 
+import pandas as pd
+
+### recieve command line arguments
 
 parser = argparse.ArgumentParser(description='sample aware read alignment')
 
-parser.add_argument('-o', '--outDir', action='store', nargs=1, default=os.getcwd(), type=str, required=True, help="output directory")
-parser.add_argument('-t', '--threads', action='store', nargs=1, default=1, type=int, required=False, help="number of cores")
-parser.add_argument('--paired', action='store_true', nargs=0, type=bool, required=False, help="if paired run")
+parser.add_argument('--outDir', action='store', nargs=1, default=os.getcwd(), type=str, required=True, help='output directory')
+parser.add_argument('--threads', action='store', nargs=1, default=1, type=int, required=False, help='number of cores')
+parser.add_argument('--sampleSheet', actions='store', nargs=1, type=str, required=True, help='sample sheet file')
 
 args = parser.parse_args()
 
-with open('samples.txt') as f:
-	samples = [x.strip().split('\t') for x in file if not x.startswith('sample_ID')]
+### functions
 
-if args.paired:
-	for sample in samples:
-		command = str(
+def align_experimental(row):
+	if row['paired'].lower() == 'paired':
+		command = [
+			'bowtie2',
+			'-x', os.path.join(args.outdir, 'genome', 'hg38'),
+			'-1', row['R1'], '-2', row['R2'],
+			'-S', os.path.join(args.outdir, 'results', 'aligned', row['sampleID'] + '_' + row['condition'] + '_' + row['replicate'] + '.sam'),
+			'-q --phred33 --no-mixed --no-discordant --threads', args.threads
+		]
+		subprocess.run(' '.join(command), shell=True, check=True)
+	else:
+		command = [
+                        'bowtie2',
+                        '-x', os.path.join(args.outdir, 'genome', 'hg38'),
+                        '-U', row['R1'],
+                        '-S', os.path.join(args.outdir, 'results', 'aligned', row['sampleID'] + '_' + row['condition'] + '_' + row['replicate'] + '.sam'),
+                        '-q --phred33 --threads', args.threads
+                ]
+                subprocess.run(' '.join(command), shell=True, check=True)
+
+def align_controls(row):
+        if row['paired'].lower() == 'paired':
+                command = [
+                        'bowtie2',
+                        '-x', os.path.join(args.outdir, 'genome', 'hg38'),
+                        '-1', row['R1_control'], '-2', row['R2_control'],
+                        '-S', os.path.join(args.outdir, 'results', 'aligned', row['condition'] + '_' + row['replicate'] + '_control.sam'),
+                        '-q --phred33 --no-mixed --no-discordant --threads', args.threads
+                ]
+                subprocess.run(' '.join(command), shell=True, check=True)
+        else:
+                command = [
+                        'bowtie2',
+                        '-x', os.path.join(args.outdir, 'genome', 'hg38'),
+                        '-U', row['R1'],
+                        '-S', os.path.join(args.outdir, 'results', 'aligned', row['condition'] + '_' + row['replicate'] + '_control.sam'),
+                        '-q --phred33 --threads', args.threads
+                ]
+                subprocess.run(' '.join(command), shell=True, check=True)
+
+### load sample sheet as pandas object
+
+samples = pd.read_csv(args.sampleSheet, sep='\t', header=0, index_col=False)
+samples.apply(align_experimental, axis=1)
+unique_controls = samples.drop_duplicates('R1_control', keep='first')
+unique_controls.apply(align_controls, axis=1)
