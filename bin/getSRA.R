19c1016703053c9742ecb062f23652ac8b1a1b66
rpolicastro <rpolicas@indiana.edu>
2019-05-22 14:08:31 -0400

    adding ability to donwload paired end SRA files

--- bin/getSRA.R
+++ bin/getSRA.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 
 library("getopt")
-library("dplyr")
+library("tidyverse")
 
 ## command line arguments
 ## ----------------------
@@ -14,20 +14,46 @@ options <- matrix(c(
 
 opt <- getopt(options)
 
+## functions
+## ---------
+
+grab.sra <- function(row) {
+	# download R1 and split file if paired end
+	if (!is.na(row["R2"])) {
+		sra.id <- row["R1"] %>% substr(., 1, nchar(.)-8)
+		command <- paste("fastq-dump", "--split-files", sra.id)
+		system(command)
+	} else {
+		sra.id <- row["R1"] %>% substr(., 1, nchar(.)-6)
+		command <- paste("fastq-dump", sra.id)
+		system(command)
+	}
+
+	# download control
+	downloaded.controls <- c()
+	if (!is.na(row["control_ID"]) & !(row["control_ID"] %in% downloaded.controls)) {
+		if (!is.na(row["R2_control"])) {
+			sra.id <- row["R1_control"] %>% substr(., 1, nchar(.)-8)
+			command <- paste("fastq-dump", "--split-files", sra.id)
+			system(command)
+			downloaded.controls <- c(downloaded.controls, row["control_ID"])
+			
+		} else {
+			sra.id <- row["R1_control"] %>% substr(., 1, nchar(.)-6)
+			command <- paste("fastq-dump", sra.id)
+			downloaded.controls <- c(downloaded.controls, row["control_ID"])
+		}
+	}
+}
+
 ## grabbing files
 ## --------------
 
 if (opt$download == "TRUE") {
 	# getting sample names
-	samples <- read.delim(opt$samplesheet, sep="\t", header=TRUE, stringsAsFactors=FALSE)
-	samples <- c(samples$R1, samples$R2, samples$R1_control, samples$R2_control)
-	samples <- samples[!is.na(samples)] %>% unique
-	samples <- substr(samples, 1, nchar(samples)-6)
-
+	samples <- read.delim(opt$samplesheet, sep="\t", header=TRUE, stringsAsFactors=FALSE) %>% as_tibble
+	
 	# downloading files
 	setwd(opt$seqdir)
-	for (sample in samples) {
-		command <- paste("fastq-dump", sample)
-		system(command)
-	}
+	apply(samples, 1, grab.sra())
 }
